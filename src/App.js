2832f597ebce396f14029716a12297d4132af228
WalukaUI <cwaluka@yahoo.com>
2022-12-07 23:52:57 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -4,20 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hdfsdfs</h1>
+      <h2>sdfsf</h2>
     </div>
   );
 }
